0d230df3ce387a943013b60469853318a7b41017
minsuhkim <124859537+minsuhkim@users.noreply.github.com>
2025-02-03 15:12:44 +0900

    2차원배열

--- Program.cs
+++ Program.cs
@@ -4,18 +4,23 @@
     {
         static void Main(string[] args)
         {
-            int size = 5;
+            int size = 10;
+            int[,] data = new int[size,size];
+            
+            for(int i=0; i<size; i++)
+            {
+                for(int j=0; j<size; j++)
+                {
+                    data[i, j] = i * 10 + j + 1;
+                }
+            }
 
             for (int i = 0; i < size; i++)
             {
-                Console.Write($"{i + 1}열:");
-                for (int j = 0; j < size - (i+1); j++)
+                for (int j = 0; j < size; j++)
                 {
-                    Console.Write(" ");
-                }
-                for (int j = 0; j <= i; j++)
-                {
-                    Console.Write('*');
+                    Console.Write(data[i, j]);
+                    Console.Write('\t');
                 }
                 Console.WriteLine();
             }
